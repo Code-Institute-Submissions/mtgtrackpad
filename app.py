6f6793953f5e3c903eadfa2f3588b067de3ace7d
JPMurdie <jpmurdie@gmail.com>
2020-06-04 15:53:07 +0000

    Recent Events table added to homepage and reading data from mongodb. More detailed data looping throughout accordion via bootstrap

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-
 from os import path
 if path.exists("env.py"):
     import env
@@ -11,13 +10,15 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGODB_DBNAMEs")
 app.config["MONGO_URI"] = os.environ.get("MONGODB_URIs")
+
 mongo = PyMongo(app)
 
 
 @app.route('/')
 @app.route('/homepage')
 def homepage():
-    return render_template("homepage.html")
+    return render_template("homepage.html",
+    records=mongo.db.Player_Records.find())
 
 
 @app.route('/new_record')
