a8364d6e5edd6b04cf627c05837b782cb0bdb636
JPMurdie <jpmurdie@gmail.com>
2020-06-12 08:38:07 +0000

    Seperated forms to forms.py to clean up app.py. Will restructure forms and remove dynamic round entery due to mvp constraits.

--- app.py
+++ app.py
@@ -1,50 +1,24 @@
 import os
 from flask import Flask, render_template, redirect, url_for, request
-from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, PasswordField, BooleanField, TextField, SubmitField, IntegerField
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm, RecaptchaField
-from wtforms.validators import InputRequired, Email, Length
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
 from wtforms.fields.html5 import DateField
+from forms import NewEvent, NewRound
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-from datetime import datetime
 from os import path
 if path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
 
 
-@app.template_filter('datetimeformat')
-def datetimeformat(value, format='%B'):
-    return value.strftime(format)
-
-
-app.jinja_env.filters['datetimeformat'] = datetimeformat
-
-
-app.config["MONGO_DBNAME"] = os.environ.get("MONGODB_DBNAMEs")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAMEs")
 app.config["MONGO_URI"] = os.environ.get("MONGODB_URIs")
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEYs")
 
+
 mongo = PyMongo(app)
-
-
-class NewEvent(FlaskForm):
-    player_name = StringField('Your Name')
-    mtgformat = StringField('Format')
-    deck_name = StringField('Deck Name')
-    event_date = DateField("Date", format='%Y-%m-%d')
-
-
-class NewRound(FlaskForm):
-    event_rounds = IntegerField('Round')
-    oppname = StringField('Opponent Name')
-    oppdeck = StringField('Deck Name')
-    roundwins = IntegerField("Games Won")
-    rounddraws = IntegerField("Games Drawn")
-    roundloss = IntegerField("Games Lost")
 
 
 @app.route('/new_record', methods=['GET', 'POST'])
@@ -57,7 +31,23 @@ def new_record():
 @app.route('/add_record', methods=['POST'])
 def add_record():
     records = mongo.db.Player_Records
-    records.insert_one(request.form.to_dict())
+    formdata = [{
+        'player_name': request.form.get('player_name'),
+        'mtgformat': request.form.get('mtgformat'),
+        'deck_name': request.form.get('deck_name'),
+        'event_date': request.form.get('event_date'),
+        'event_rounds': [
+            {
+                'round': 1,
+                'opp_name': request.form.get('oppname'),
+                'opp_deck': request.form.get('oppdeck'),
+                'game_wins': request.form.get('roundwins'),
+                'game_draws': request.form.get('rounddraws'),
+                'game_loss': request.form.get('roundloss')
+            }
+        ]
+    }]
+    records.insert_many(formdata)
     return redirect(url_for('homepage'))
 
 
@@ -65,7 +55,7 @@ def add_record():
 @app.route('/homepage')
 def homepage():
     return render_template("homepage.html",
-                           records=mongo.db.Player_Records.find().sort("event_date", -1))
+                           records=mongo.db.Player_Records.find())
 
 
 @app.route('/player_history')
@@ -73,7 +63,7 @@ def player_history():
     return render_template("playerhistory.html")
 
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP", "0.0.0.0"),
+            port=int(os.environ.get("PORT", "5000")),
             debug=True)
---
+++ forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from datetime import datetime
+from wtforms import StringField, IntegerField, PasswordField, SubmitField, BooleanField, DateField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+
+class NewEvent(FlaskForm):
+    player_name = StringField('Your Name')
+    mtgformat = StringField('Format')
+    deck_name = StringField('Deck Name')
+    event_date = DateField("Date", format='%d-%m-%Y')
+
+
+class NewRound(FlaskForm):
+    event_rounds = IntegerField('Round')
+    oppname = StringField('Opponent Name')
+    oppdeck = StringField('Deck Name')
+    roundwins = IntegerField("Games Won")
+    rounddraws = IntegerField("Games Drawn")
+    roundloss = IntegerField("Games Lost")
